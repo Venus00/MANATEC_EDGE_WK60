1dc1698e9d77d0d4c0c7b8c56f0106ad50b20c0f
Venus00 <oussama@nextronic.io>
2023-11-24 20:45:37 +0100

    mpubluasDKLHWQSF

--- src/mqtt/mqtt.module.ts
+++ src/mqtt/mqtt.module.ts
@@ -1,10 +1,10 @@
-import { Module } from '@nestjs/common';
+import { Module, forwardRef } from '@nestjs/common';
 import { MqttService } from './mqtt.service';
 import { PrismaModule } from 'src/prisma/prisma.module';
 import { EventModule } from 'src/event/event.module';
 import { SerialModule } from 'src/serial/serial.module';
 @Module({
-  imports:[PrismaModule,EventModule,SerialModule],
+  imports:[PrismaModule,EventModule,forwardRef(() => SerialModule)],
   providers: [MqttService],
   exports:[MqttService]
 })
--- src/serial/serial.module.ts
+++ src/serial/serial.module.ts
@@ -1,11 +1,11 @@
-import { Module } from '@nestjs/common';
+import { Module, forwardRef } from '@nestjs/common';
 import { SerialService } from './serial.service';
 import { PrismaModule } from 'src/prisma/prisma.module';
 import { EventModule } from 'src/event/event.module';
 import { DeltaModule } from 'src/delta/delta.module';
 import { MqttModule } from 'src/mqtt/mqtt.module';
 @Module({
-  imports: [EventModule,PrismaModule,DeltaModule,MqttModule],
+  imports: [EventModule,PrismaModule,DeltaModule,forwardRef(() => MqttModule)],
   providers: [SerialService],
   exports:[SerialService]
 })
