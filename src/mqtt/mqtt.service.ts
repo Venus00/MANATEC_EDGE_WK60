6f6a08e53dcf31d2ba2fc9a97e63deb8dc68d79b
Venus00 <oussama@nextronic.io>
2023-12-18 16:48:44 +0100

    fix

--- src/mqtt/mqtt.service.ts
+++ src/mqtt/mqtt.service.ts
@@ -24,8 +24,8 @@ export class MqttService implements OnModuleInit {
     this.logger.log(process.env.MQTT_SERVER)
     this.client = mqtt.connect(`mqtt://${process.env.MQTT_SERVER}}`, {
       clientId: this.mac,
-      username: this.mac,
-      password: this.mac,
+      // username: this.mac,
+      // password: this.mac,
       keepalive: 1,
       reconnectPeriod: 1
     });
