e32eeb65533c88432a932227e4140ac804b95c54
Venus00 <oussama@nextronic.io>
2023-12-18 12:55:03 +0100

    fdux

--- src/status/status.service.ts
+++ src/status/status.service.ts
@@ -38,7 +38,7 @@ export class StatusService {
                 name:'stauts',
             },
             data:{
-                shut,
+                shut:shut,
             }
         })
        }
